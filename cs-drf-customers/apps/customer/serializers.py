7d0d7919742425159285dbe6f2df59d616e6bada
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-10-16 10:30:03 -0300

    feat: created custom validate in serializer

--- cs-drf-customers/apps/customer/serializers.py
+++ cs-drf-customers/apps/customer/serializers.py
@@ -5,3 +5,8 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def validate_cpf(self, value):
+        if len(value) != 11:
+            raise serializers.ValidationError('O cpf deve ter 11 dígitos.')
+        return value
