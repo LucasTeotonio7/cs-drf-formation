fa608b6e432f67ed12cbf7a31db93a28ee50f75e
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-13 19:18:51 -0300

    feat: added validate docbr

--- cs-drf-customers/apps/customer/serializers.py
+++ cs-drf-customers/apps/customer/serializers.py
@@ -1,5 +1,6 @@
 import re
 from rest_framework import serializers
+from validate_docbr import CPF
 from apps.customer.models import Customer
 from apps.customer.validators import validate_min_length, list_in_one_dict
 
@@ -7,6 +8,12 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
+
+    def validate_cpf(self, value):
+        cpf = CPF()
+        if not cpf.validate(value):
+            raise serializers.ValidationError('Número de cpf inválido')
+        return value
 
     def validate_name(self, value):
         if not value.isalpha():
@@ -20,18 +27,18 @@ class CustomerSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Formato inválido, formato aceito: 99 99999-9999.')
 
 
-    def validate(self, data):
-        print("cpf")
-        errors_list = []
+    # def validate(self, data):
+    #     print("cpf")
+    #     errors_list = []
 
-        errors_list.append(validate_min_length('cpf', data.get('cpf'), 11))
-        errors_list.append(validate_min_length('rg', data.get('rg'), 9))
+    #     errors_list.append(validate_min_length('cpf', data.get('cpf'), 11))
+    #     errors_list.append(validate_min_length('rg', data.get('rg'), 9))
 
-        qty_errors = 0
-        for error in errors_list:
-            if error is not None:
-                qty_errors =+ 1
+    #     qty_errors = 0
+    #     for error in errors_list:
+    #         if error is not None:
+    #             qty_errors =+ 1
 
-        if qty_errors > 0:
-            raise serializers.ValidationError(list_in_one_dict(errors_list))
-        return data
+    #     if qty_errors > 0:
+    #         raise serializers.ValidationError(list_in_one_dict(errors_list))
+    #     return data
