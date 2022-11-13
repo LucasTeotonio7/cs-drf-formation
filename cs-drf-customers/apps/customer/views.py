66bbbad780e472fca241db946e7f97604fdd2f71
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-13 20:23:33 -0300

    feat: created name, cpf and active filter.

--- cs-drf-customers/apps/customer/views.py
+++ cs-drf-customers/apps/customer/views.py
@@ -7,5 +7,7 @@ class CustomerViewSet(viewsets.ModelViewSet):
     """customer list"""
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter ]
     ordering_fields = ['name']
+    search_fields = ['name', 'cpf']
+    filterset_fields = ['active']
