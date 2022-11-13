730d5b9f1dee2b9781570fb85b84d93767f31749
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-13 20:41:20 -0300

    feat: added authentication

--- cs-drf-customers/apps/customer/views.py
+++ cs-drf-customers/apps/customer/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from apps.customer.serializers import CustomerSerializer
 from apps.customer.models import Customer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class CustomerViewSet(viewsets.ModelViewSet):
     """customer list"""
@@ -11,3 +13,6 @@ class CustomerViewSet(viewsets.ModelViewSet):
     ordering_fields = ['name']
     search_fields = ['name', 'cpf']
     filterset_fields = ['active']
+    authentication_class = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
