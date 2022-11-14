f527565276ece81135868cbd38cdd3706c7c5b64
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-14 15:33:31 -0300

    feat: added django model permissions

--- cs-drf-api-school/apps/school/auth/authentication_mixin.py
+++ cs-drf-api-school/apps/school/auth/authentication_mixin.py
@@ -1,7 +1,7 @@
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 
 class AuthMixin():
     authentication_class = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
