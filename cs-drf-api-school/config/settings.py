0d785ced35955ad2e06aac9396d3472ed64e53d3
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-14 16:12:36 -0300

    feat: limiting number of anonymous user requests

--- cs-drf-api-school/config/settings.py
+++ cs-drf-api-school/config/settings.py
@@ -144,5 +144,11 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication'
-    ]
+    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle'
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '5/day',
+    # }
 }
