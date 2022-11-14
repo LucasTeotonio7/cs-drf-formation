c3efe8bf00f4e2ac77a714c370a2c3fe55f12d00
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-14 18:25:48 -0300

    feat: added location for post requests

--- cs-drf-api-school/apps/school/views/views_course.py
+++ cs-drf-api-school/apps/school/views/views_course.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from apps.school.auth import AuthMixin
 from apps.school.models import Course
@@ -8,3 +9,12 @@ from apps.school.serializers import CourseSerializer
 class CourseViewSet(AuthMixin, viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
