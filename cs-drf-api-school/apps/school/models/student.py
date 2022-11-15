b6d6fadd86b5021b6d0001ec0521d99b086a1e01
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-15 14:11:25 -0300

    feat: added photo in student model

--- cs-drf-api-school/apps/school/models/student.py
+++ cs-drf-api-school/apps/school/models/student.py
@@ -8,6 +8,7 @@ class Student(models.Model):
     cpf = models.CharField(unique=True, max_length=11)
     birth_date = models.DateField()
     phone = models.CharField(max_length=11, default="")
+    photo = models.ImageField(blank=True)
 
 
     class Meta:
