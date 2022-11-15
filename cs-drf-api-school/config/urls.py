384e6ad3d54634d5b371d8642b6869c5d041fd84
lucasteotonio7 <lucas-santos-teo@hotmail.com>
2022-11-15 14:12:30 -0300

    chore: configured file directory

--- cs-drf-api-school/config/urls.py
+++ cs-drf-api-school/config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import routers
 
@@ -17,4 +19,4 @@ urlpatterns = [
     path('', include(router.urls)),
     path('students/<int:pk>/enrollments/', EnrollmentStudentListViewSet.as_view()),
     path('courses/<int:pk>/enrollments/', ListStudentsEnrolledInCourseViewSet.as_view()),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
