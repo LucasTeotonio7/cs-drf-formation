302b71cda78c29bd63ab6b42d026cc3b35f23641
lucasteotonio <lucas.teotonio@e-comprei.com>
2022-11-16 21:43:53 -0300

    feat: created get and post tests in courses

---
+++ cs-drf-api-school/tests/test_courses.py
@@ -0,0 +1,36 @@
+from django.urls import reverse
+
+from rest_framework.test import APITestCase
+from rest_framework import status
+
+from apps.school.models import Course
+
+
+
+class CourseTestCase(APITestCase):
+
+    def setUp(self) -> None:
+        self.list_url = reverse('courses-list')
+        self.course1 = Course.objects.create(
+            code="TEST-1", 
+            description="Course test 1", 
+            level=Course.Level.BASIC
+        )
+        self.course2 = Course.objects.create(
+            code="TEST-2", 
+            description="Course test 2", 
+            level=Course.Level.INTERMEDIARY
+        )
+    
+    def test_get_request_to_list_courses(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_post_request_to_create_course(self):
+        data = {
+            "code":"TEST-3", 
+            "description":"Course test 3", 
+            "level": Course.Level.ADVANCED
+        }
+        response = self.client.post(self.list_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
